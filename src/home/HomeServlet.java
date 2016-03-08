bb473eaadef3213b1d2233afb3a899be44c2c676
wywant <wywant@sina.com>
2016-03-08 16:30:53 +0800

    all

---
+++ src/home/HomeServlet.java
@@ -0,0 +1,12 @@
+package home;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HomeServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
